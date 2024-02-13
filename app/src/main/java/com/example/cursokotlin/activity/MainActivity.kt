b6bea5ba5623a0247af932cb49db4bf9a13a5349
Jheiderk <jheiderk@gmail.com>
2024-02-13 11:13:44 +0100

    -Ahora se puede ajustar el peso dejando el boton presionado

--- app/src/main/java/com/example/cursokotlin/activity/MainActivity.kt
+++ app/src/main/java/com/example/cursokotlin/activity/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.cursokotlin.activity
 
-import android.graphics.Color
+
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.CountDownTimer
+import android.view.MotionEvent
+import android.view.TextureView
 import android.widget.Button
-import android.widget.EditText
-import android.widget.SeekBar
 import android.widget.TextView
 import com.example.cursokotlin.R
-import com.example.cursokotlin.R.color
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +27,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnImc: Button
     private lateinit var resultTextTv:TextView
     private lateinit var descriptionResult:TextView
+    private var countDownTimer: CountDownTimer? = null
+    private lateinit var WeightText:TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +40,42 @@ class MainActivity : AppCompatActivity() {
         view()
         heightCm()
         weightKg()
+        touch()
+        press()
 
-        slider.addOnChangeListener { _, value, _ ->
-            heightCurrent = value.toInt()
-            heightCm()
-        }
-        rest.setOnClickListener {
-            weight++
-            weightKg()
-        }
-        plus.setOnClickListener {
-            weight--
-            weightKg()
-        }
-        btnImc.setOnClickListener {
-            calculateBMI()
-        }
+
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun press() {
+        plus.setOnTouchListener { _, press ->
+            when(press.action) {
+                MotionEvent.ACTION_DOWN -> startTimer(true)
+                MotionEvent.ACTION_UP -> stopTimer()
+
+            }
+            true
+        }
+        rest.setOnTouchListener { _, press ->
+            when(press.action){
+                MotionEvent.ACTION_DOWN -> startTimer(false)
+                MotionEvent.ACTION_UP -> stopTimer()
+            }
+            true
+        }
+    }private fun startTimer(increment: Boolean) {
+        countDownTimer=object: CountDownTimer(Long.MAX_VALUE, 150){
+            override fun onTick(millisUntilFinished: Long) {
+                if(increment) weight++
+                else weight--
+
+                weightKg()
+            }
+            override fun onFinish() {}
+        }
+        countDownTimer?.start()
+    }
+
     private fun view() {
         heightTv = findViewById(R.id.texto2)
         rest = findViewById(R.id.restFab)
@@ -60,22 +84,25 @@ class MainActivity : AppCompatActivity() {
         btnImc = findViewById(R.id.button)
         resultTextTv= findViewById(R.id.resultTextTv)
         descriptionResult=findViewById(R.id.descriptionResult)
+        WeightText= findViewById(R.id.texto3)
     }
+    private fun stopTimer()  {
+        countDownTimer?.cancel()
 
+    }
     private fun heightCm() {
         heightTv.text = " $heightCurrent cm"
     }
 
-    fun weightKg() {
-        var WeightText: TextView = findViewById(R.id.texto3)
+    private fun weightKg() {
         WeightText.text = "$weight Kg"
     }
 
     private fun calculateBMI() {
-        var result = (weight/ (heightCurrent.toFloat() / 100).pow(2))
+        val result = (weight/ (heightCurrent.toFloat() / 100).pow(2))
         val formatter:DecimalFormat= DecimalFormat("#.##")
-        var description:String?
-        var descriptionColor:Int?
+        val description:String?
+        val descriptionColor:Int?
         when(result){
             in 0.0..18.5-> {
                 description=getString(R.string.low_weight)
@@ -102,5 +129,15 @@ class MainActivity : AppCompatActivity() {
         descriptionResult.text=(description)
         descriptionResult.setTextColor(descriptionColor)
         resultTextTv.setTextColor(descriptionColor)
+    }
+    private fun touch() {
+        slider.addOnChangeListener { _, value, _ ->
+            heightCurrent = value.toInt()
+            heightCm()
+        }
+
+        btnImc.setOnClickListener {
+            calculateBMI()
+        }
     }
 }
